62f0338068124feee63b722573e9395d9642ad2a
ErikSNyborg <esnv96@gmail.com>
2023-07-30 01:35:28 +0200

    Some work done on day 1

--- solutionsSleddersquid/day_01/arrays.js
+++ solutionsSleddersquid/day_01/arrays.js
@@ -5,7 +5,7 @@
 const arr1 = Array();
 const arr2 = new Array();
 // Using squarebrackets. This is the recommended way to create an empty list
-const arr3 = [];
+const arr3 = [1, 2, 3, 4];
 
 // Creating arrays with different conten inside
 const numbers = [0, 3.14, 9.81, 37, 98.6, 100] // array of numbers
@@ -34,3 +34,15 @@ console.log('Number of web technologies:', webTechs.length)
 
 console.log('Countries:', countries)
 console.log('Number of countries:', countries.length)
+
+// Arrays kan also have different items, data types and even arrays
+const manytypesArray = [
+    'Asabeneh',
+    250,
+    true,
+    { country: 'Finland', city: 'Helsinki' },
+    { skills: ['HTML', 'CSS', 'JS', 'React', 'Python'] },
+  ] // arr containing different data types
+  console.log(manytypesArray[3]); // { country: 'Finland', city: 'Helsinki' }
+  console.log(manytypesArray[3]["country"]); // Finland
+
---
+++ solutionsSleddersquid/day_01/stringmanipulation.js
@@ -0,0 +1,13 @@
+// We can create arrays from strings
+let jsString = "JavaScript";
+// Split separates a string into an array
+const charsInJavascript = jsString.split(""); // ["J", "a", "v", "a", "S", "c", "r", "i", "p", "t"]
+console.log(charsInJavascript);
+
+// split kan also be used to split when a spesific character is present in the string
+let companiesString = 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon'
+// This splits the string
+const companies = companiesString.split(',');
+console.log(companies) // ["Facebook", " Google", " Microsoft", " Apple", " IBM", " Oracle", " Amazon"]
+
+let text = "I love working with JavaScript, CSS and HTML. But "
