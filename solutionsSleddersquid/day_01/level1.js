b45dd6a99616c5463b9b397df6de956c65c08d9b
ErikSNyborg <esnv96@gmail.com>
2023-07-30 00:40:20 +0200

    Some work done on day 1

--- solutionsSleddersquid/day_01/level1.js
+++ solutionsSleddersquid/day_01/level1.js
@@ -1,15 +1,36 @@
-const array = [1, 2, 3, 4, 5]
+// let and const are block scope
+// var is a function scope
 
-let sum = 0
-array.forEach((n) => {
-    sum +=n;
-});
+// Arrays
+const arr1 = Array();
+const arr2 = new Array();
+// Using squarebrackets. This is the recommended way to create an empty list
+const arr3 = [];
 
-console.log(`Sum of array [${array}] is ` + sum);
+// Creating arrays with different conten inside
+const numbers = [0, 3.14, 9.81, 37, 98.6, 100] // array of numbers
+const fruits = ['banana', 'orange', 'mango', 'lemon'] // array of strings, fruits
+const vegetables = ['Tomato', 'Potato', 'Cabbage', 'Onion', 'Carrot'] // array of strings, vegetables
+const animalProducts = ['milk', 'meat', 'butter', 'yoghurt'] // array of strings, products
+const webTechs = ['HTML', 'CSS', 'JS', 'React', 'Redux', 'Node', 'MongDB'] // array of web technologies
+const countries = ['Finland', 'Denmark', 'Sweden', 'Norway', 'Iceland'] // array of strings, countries
 
-// It companies
-const itCompanies = ["Facebook", "Google", "Microsoft", "Apple", "IBM", "Oracle", "Amazon"];
+// Print the array and its length
 
-itCompanies.forEach((element) => {
-    console.log(element);
-})
+console.log('Numbers:', numbers)
+console.log('Number of numbers:', numbers.length)
+
+console.log('Fruits:', fruits)
+console.log('Number of fruits:', fruits.length)
+
+console.log('Vegetables:', vegetables)
+console.log('Number of vegetables:', vegetables.length)
+
+console.log('Animal products:', animalProducts)
+console.log('Number of animal products:', animalProducts.length)
+
+console.log('Web technologies:', webTechs)
+console.log('Number of web technologies:', webTechs.length)
+
+console.log('Countries:', countries)
+console.log('Number of countries:', countries.length)
